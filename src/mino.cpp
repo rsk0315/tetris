f057c30d7c279aeae474bef01bf1342bb56be66e
rsk0315 <rsk0315@icloud.com>
2019-06-04 13:30:28 +0900

    Has many bugs

--- src/mino.cpp
+++ src/mino.cpp
@@ -1,12 +1,15 @@
 #include <cstdio>
 #include <cstdint>
 #include <cstddef>
+#include <cctype>
 #include <cassert>
 #include <algorithm>
 #include <array>
 #include <stdexcept>
 #include <tuple>
 #include <utility>
+#include <string>
+#include <random>
 
 class game;
 class board;
@@ -14,7 +17,7 @@ class mino;
 
 class board {
   friend game;
-  static size_t constexpr S_rows = 20;
+  static size_t constexpr S_rows = 21;
   static size_t constexpr S_columns = 10;
   static size_t constexpr S_margin = 3;
   using board_type = std::array<std::array<int, S_columns+2*S_margin>, S_rows+2*S_margin>;
@@ -26,8 +29,16 @@ public:
   board(board const&) = default;
   board(board&&) = default;
 
+  bool out_of_range(size_t i, size_t j) const {
+    return (!(i < S_rows && j < S_columns));
+  }
+
   bool occupied(size_t i, size_t j) const {
     return M_board[S_margin+i][S_margin+j];
+  }
+
+  void set(size_t i, size_t j, char) {
+    M_board[S_margin+i][S_margin+j] = 1;
   }
 };
 
@@ -39,7 +50,7 @@ class mino {
   block_type M_block;
   int M_state = 0;
 
-  size_t M_i = 0, M_j = 5;
+  size_t M_i = 0, M_j = 3;
 
   void M_block_init() {
     switch (M_type) {
@@ -58,7 +69,7 @@ class mino {
                              {1, 1, 1, 0},
                              {0, 0, 0, 0},
                              {0, 0, 0, 0}} }; break;
-    case '0':
+    case 'O':
       M_block = block_type{ {{0, 1, 1, 0},
                              {0, 1, 1, 0},
                              {0, 0, 0, 0},
@@ -143,13 +154,24 @@ public:
   mino(char type): M_type(type) { M_block_init(); }
 
   bool move(ptrdiff_t di, ptrdiff_t dj, board const& base) {
-    M_i += di;
-    M_j += dj;
-    return true;  // XXX
+    size_t next_i = M_i + di;
+    size_t next_j = M_j + dj;
+    {
+      size_t frame = 4;
+      for (size_t i = 0; i < frame; ++i)
+        for (size_t j = 0; j < frame; ++j) {
+          if (!M_block[i][j]) continue;
+          if (base.out_of_range(next_i+i, next_j+j)) return false;
+          if (base.occupied(next_i+i, next_j+j)) return false;
+        }
+    }
+    M_i = next_i;
+    M_j = next_j;
+    return true;
   }
 
-  std::pair<ptrdiff_t, ptrdiff_t> rotate(bool clockwise, board const& base) {
-    if (M_type == 'O') return {0, 0};
+  bool rotate(bool clockwise, board const& base) {
+    if (M_type == 'O') return true;
 
     block_type next_block = M_block;
     {
@@ -171,6 +193,7 @@ public:
     }
 
     int next_state = M_state + (clockwise? +1:-1);
+    (next_state += 4) %= 4;
     {
       /* check */
       for (auto const& offset: M_offset_array(clockwise)) {
@@ -180,7 +203,7 @@ public:
         for (size_t i = 0; i < 4; ++i)
           for (size_t j = 0; j < 4; ++j) {
             if (!next_block[i][j]) continue;
-            if (base.occupied(M_i+i+i0, M_j+j+j0)) {
+            if (base.out_of_range(M_i+i+i0, M_j+j+j0) || base.occupied(M_i+i+i0, M_j+j+j0)) {
               valid = false;
               break;
             }
@@ -189,12 +212,14 @@ public:
         if (valid) {
           M_state = next_state;
           M_block = next_block;
-          return offset;
+          M_i += i0;
+          M_j += j0;
+          return true;
         }
       }
     }
 
-    throw std::logic_error("cannot rotate");
+    return false;
   }
 
   bool occupies(size_t i, size_t j) const {
@@ -211,23 +236,120 @@ public:
 
 class game {
   board b;
-  
-  void inspect(board const& b, mino const& m) const {
+  std::string const S_minos = "ILJOSZT";
+  std::string M_minos = S_minos;
+  std::mt19937 M_rng;
+
+  void M_inspect(board const& b, mino const& m) const {
+    fprintf(stderr, "%c-mino: (%zu, %zu)\n", m.M_type, m.M_i, m.M_j);
     for (size_t i = 0; i < b.S_rows; ++i)
       for (size_t j = 0; j < b.S_columns; ++j) {
         char cur = '.';
-        if (b.M_board[i+b.S_margin][j+b.S_margin]) cur = '#';
         if (m.occupies(i, j)) cur = '=';
+        if (b.M_board[i+b.S_margin][j+b.S_margin]) cur = '#';
         fprintf(stderr, "%c%c", cur, j+1<b.S_columns? ' ':'\n');
       }
   }
+  
+  void M_reset_minos() {
+    M_minos = S_minos;
+    std::shuffle(M_minos.begin(), M_minos.end(), M_rng);
+    M_minos = "ISZOSLJTT";
+    std::reverse(M_minos.begin(), M_minos.end());
+  }
+
+  char S_get_op() const {
+    char res;
+    do {
+      res = getchar();
+      fprintf(stderr, "op: %c\n", res);
+    } while (!(res == EOF || !isspace(res)));
+    return res;
+  }
+
+  bool M_set_mino(mino const& m, board& b) {
+    {
+      /* check */
+      bool valid = false;
+      for (size_t i = 0; i < 4; ++i)
+        for (size_t j = 0; j < 4; ++j) {
+          if (!m.M_block[i][j]) continue;
+          size_t bi = m.M_i + i;
+          size_t bj = m.M_j + j;
+          if (b.out_of_range(bi+1, bj) || b.occupied(bi+1, bj)) {
+            valid = true;
+            break;
+          }
+        }
+      if (!valid) return false;
+    }
+
+    {
+      /* set */
+      for (size_t i = 0; i < 4; ++i)
+        for (size_t j = 0; j < 4; ++j) {
+          if (!m.M_block[i][j]) continue;
+          size_t bi = m.M_i + i;
+          size_t bj = m.M_j + j;
+          b.set(bi, bj, m.M_type);
+        }
+    }
+    return true;
+  }
+
+  void M_put_mino() {
+    fprintf(stderr, "putting: %c\n", M_minos.back());
+    mino m(M_minos.back());
+    M_minos.pop_back();
+
+    M_inspect(b, m);
+
+    while (true) {
+      char op = S_get_op();
+      bool valid = true;
+      switch (op) {
+      case 'j':
+        valid = m.move(+1, 0, b);
+        break;
+      case 'k':
+        valid = m.move(-1, 0, b);
+        break;
+      case 'h':
+        valid = m.move(0, -1, b);
+        break;
+      case 'l':
+        valid = m.move(0, +1, b);
+        break;
+
+      case 'J':
+        while (m.move(+1, 0, b)) {}
+        M_set_mino(m, b);
+        M_inspect(b, m);
+        return;
+
+      case 'f':
+        valid = m.rotate(true, b);
+        break;
+      case 'd':
+        valid = m.rotate(false, b);
+        break;
+      }
+
+      fprintf(stderr, valid? "valid\n":"invalid\n");
+      M_inspect(b, m);
+    }
+  }
 
 public:
-  game() = default;
+  game() {
+    M_reset_minos();
+  }
 
   void play() {
-    mino i('I');
-    inspect(b, i);
+    do {
+      if (M_minos.empty()) M_reset_minos();
+      M_put_mino();
+    } while (true);
   }
 };
 
